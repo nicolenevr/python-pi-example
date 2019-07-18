0d4eb2e399326ef7ef251af4c9b94bd396202972
nicolenevr <nicolenevr27@gmail.com>
2019-07-18 09:10:46 -0700

    new changes

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 
 def main(mgs):
-    // another comment
-    // adding a different comment to this file
-    // prints the message entered into the definition function
     print(mgs)
 
 
+def secondary():
+    print("New Function")
+
 main("Hello World!")
+secondary()
